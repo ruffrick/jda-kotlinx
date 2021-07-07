254e707b533206111c21b0447ed35b6d97168706
ruffrick <henrik-eckhardt@t-online.de>
2021-07-07 19:19:25 +0200

    Add Env delegate

--- src/main/kotlin/dev/ruffrick/jda/kotlinx/Delegates.kt
+++ src/main/kotlin/dev/ruffrick/jda/kotlinx/Delegates.kt
@@ -5,7 +5,48 @@ import org.slf4j.LoggerFactory
 import kotlin.reflect.KProperty
 
 object Logger {
-
     operator fun getValue(thisRef: Any, property: KProperty<*>): Logger = LoggerFactory.getLogger(thisRef::class.java)
+}
+
+object Env {
+
+    private val regex0 = Regex("\\W+")
+    private val regex1 = Regex("\\s+|\\B(?=[A-Z])")
+
+    private fun getEnv(thisRef: Any, property: KProperty<*>): String? {
+        val prefix = thisRef::class.java.name.replace(regex0, " ").split(regex1).joinToString("_")
+        val name = property.name.replace(regex0, " ").split(regex1).joinToString("_")
+        return System.getenv("${prefix.uppercase()}_${name.uppercase()}")
+    }
+
+    operator fun getValue(thisRef: Any, property: KProperty<*>) = getEnv(thisRef, property)
+
+    object Byte {
+        operator fun getValue(thisRef: Any, property: KProperty<*>) = getEnv(thisRef, property)?.toByte()
+    }
+
+    object Short {
+        operator fun getValue(thisRef: Any, property: KProperty<*>) = getEnv(thisRef, property)?.toShort()
+    }
+
+    object Int {
+        operator fun getValue(thisRef: Any, property: KProperty<*>) = getEnv(thisRef, property)?.toInt()
+    }
+
+    object Long {
+        operator fun getValue(thisRef: Any, property: KProperty<*>) = getEnv(thisRef, property)?.toLong()
+    }
+
+    object Float {
+        operator fun getValue(thisRef: Any, property: KProperty<*>) = getEnv(thisRef, property)?.toFloat()
+    }
+
+    object Double {
+        operator fun getValue(thisRef: Any, property: KProperty<*>) = getEnv(thisRef, property)?.toDouble()
+    }
+
+    object Boolean {
+        operator fun getValue(thisRef: Any, property: KProperty<*>) = getEnv(thisRef, property)?.toBoolean()
+    }
 
 }
